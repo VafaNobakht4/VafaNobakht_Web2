3a9a16b41357649022b659e29798dea9fd48983a
Vafa Nobakht <vafa.nobakht23@gmail.com>
2022-08-19 22:55:14 +0430

    add registration

--- src/src/Controller/UserController.php
+++ src/src/Controller/UserController.php
@@ -13,15 +13,15 @@ use Symfony\Component\Routing\Annotation\Route;
 #[Route('/user')]
 class UserController extends AbstractController
 {
-    #[Route('/', name: 'app_user_index', methods: ['GET'])]
-    public function index(UserRepository $userRepository): Response
-    {
-        return $this->render('user/index.html.twig', [
-            'users' => $userRepository->findAll(),
-        ]);
-    }
+//    #[Route('/', name: 'app_user_index', methods: ['GET'])]
+//    public function index(UserRepository $userRepository): Response
+//    {
+//        return $this->render('user/index.html.twig', [
+//            'users' => $userRepository->findAll(),
+//        ]);
+//    }
 
-    #[Route('/Register', name: 'app_user_new', methods: ['GET', 'POST'])]
+    #[Route('/register', name: 'register', methods: ['GET', 'POST'])]
     public function new(Request $request, UserRepository $userRepository): Response
     {
         $user = new User();
@@ -40,39 +40,39 @@ class UserController extends AbstractController
         ]);
     }
 
-    #[Route('/{id}', name: 'app_user_show', methods: ['GET'])]
-    public function show(User $user): Response
-    {
-        return $this->render('user/show.html.twig', [
-            'user' => $user,
-        ]);
-    }
-
-    #[Route('/{id}/edit', name: 'app_user_edit', methods: ['GET', 'POST'])]
-    public function edit(Request $request, User $user, UserRepository $userRepository): Response
-    {
-        $form = $this->createForm(UserType::class, $user);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $userRepository->add($user, true);
-
-            return $this->redirectToRoute('app_user_index', [], Response::HTTP_SEE_OTHER);
-        }
-
-        return $this->renderForm('user/edit.html.twig', [
-            'user' => $user,
-            'form' => $form,
-        ]);
-    }
-
-    #[Route('/{id}', name: 'app_user_delete', methods: ['POST'])]
-    public function delete(Request $request, User $user, UserRepository $userRepository): Response
-    {
-        if ($this->isCsrfTokenValid('delete'.$user->getId(), $request->request->get('_token'))) {
-            $userRepository->remove($user, true);
-        }
-
-        return $this->redirectToRoute('app_user_index', [], Response::HTTP_SEE_OTHER);
-    }
+//    #[Route('/{id}', name: 'app_user_show', methods: ['GET'])]
+//    public function show(User $user): Response
+//    {
+//        return $this->render('user/show.html.twig', [
+//            'user' => $user,
+//        ]);
+//    }
+//
+//    #[Route('/{id}/edit', name: 'app_user_edit', methods: ['GET', 'POST'])]
+//    public function edit(Request $request, User $user, UserRepository $userRepository): Response
+//    {
+//        $form = $this->createForm(UserType::class, $user);
+//        $form->handleRequest($request);
+//
+//        if ($form->isSubmitted() && $form->isValid()) {
+//            $userRepository->add($user, true);
+//
+//            return $this->redirectToRoute('app_user_index', [], Response::HTTP_SEE_OTHER);
+//        }
+//
+//        return $this->renderForm('user/edit.html.twig', [
+//            'user' => $user,
+//            'form' => $form,
+//        ]);
+//    }
+//
+//    #[Route('/{id}', name: 'app_user_delete', methods: ['POST'])]
+//    public function delete(Request $request, User $user, UserRepository $userRepository): Response
+//    {
+//        if ($this->isCsrfTokenValid('delete'.$user->getId(), $request->request->get('_token'))) {
+//            $userRepository->remove($user, true);
+//        }
+//
+//        return $this->redirectToRoute('app_user_index', [], Response::HTTP_SEE_OTHER);
+//    }
 }
